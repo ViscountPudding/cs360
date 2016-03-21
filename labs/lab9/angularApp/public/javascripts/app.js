0412d3b318a564bfc017707738c7170406be5ee0
ViscountPudding <stephenmccammon@comcast.net>
2016-03-21 06:46:59 +0000

    lab 9 commit

---
+++ labs/lab9/angularApp/public/javascripts/app.js
@@ -0,0 +1,50 @@
+angular.module('comment', [])
+.controller('MainCtrl', [
+  '$scope', '$http',
+  function($scope, $http){
+    $scope.test = 'Hello world!';
+    $scope.comments = [];
+
+    $scope.addComment = function() {
+      if($scope.formContent === '') { return; }
+      console.log("In addComment with "+$scope.formContent);
+
+      $scope.create({
+        title: $scope.formContent,
+        upvotes: 0,
+      });
+
+      $scope.test = 'Just added the comment: ' + $scope.formContent;
+
+      $scope.formContent = '';
+    };
+
+    $scope.incrementUpvotes = function(comment) {
+      $scope.upvote(comment);
+    };
+
+    $scope.getAll = function() {
+      return $http.get('/comments').success(function(data){
+      angular.copy(data, $scope.comments);
+      });
+    };
+
+    $scope.create = function(comment) {
+      return $http.post('/comments', comment).success(function(data){
+        $scope.comments.push(data);
+      });
+    };
+
+    
+    $scope.upvote = function(comment) {
+      return $http.put('/comments/' + comment._id + '/upvote')
+        .success(function(data){
+          console.log("upvote worked");
+          comment.upvotes += 1;
+	  $scope.test = 'Just upvoted the comment: ' + comment.title;
+        });
+    };
+
+    $scope.getAll();
+  }
+]);
