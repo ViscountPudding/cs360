e0ef55a167b949aec62c3b910917a370ae0ccb0a
ViscountPudding <stephenmccammon@comcast.net>
2016-04-02 01:07:13 +0000

    lab 10 commit

---
+++ labs/lab10/cluster/public/javascripts/angularApp.js
@@ -0,0 +1,13 @@
+angular.module('clusterApp', [])
+.controller('MainCtrl', [
+  '$scope', '$http',
+  function($scope, $http){
+    $scope.cluster = [{pid:1234}];
+
+    $scope.getPIDs = function() {
+      $scope.cluster = [{pid:12},{pid:34}];
+    }
+
+  } 
+
+]);
